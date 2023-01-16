ff77fbba04a5813e61946833d3f0017ce810a47d
Mike Hall <mike@puddingtime.org>
2023-01-15 22:07:35 -0800

    cleaning up

--- imgup.rb
+++ imgup.rb
@@ -5,83 +5,53 @@ require 'haml'
 require 'puma'
 require 'sinatra/config_file'
 require 'json'
-require 'oauth'
-require 'oauth/consumer'
+require 'rest-client'
+require 'typhoeus'
+include FileUtils::Verbose
 
 config_file 'settings.yaml'
 
 set :haml, { escape_html: false }
 set :sessions, true
 
-api_key = settings.smugmug_key
-api_secret = settings.smugmug_secret
+cloudflare_account = settings.cloudflare_account
+cloudflare_token = settings.cloudflare_token
+base_url = "https://api.cloudflare.com/client/v4/accounts/#{cloudflare_account}/images/v1"
 
-base_url = 'https://api.smugmug.com'
-request_token_path = '/services/oauth/1.0a/getRequestToken'
-access_token_path =  '/services/oauth/1.0a/getAccessToken'
-authorize_path =  '/services/oauth/1.0a/authorize'
-callback_url = "oob"
-
-
-before do
-  session[:oauth] ||= {}  
-  @consumer ||=OAuth::Consumer.new api_key, api_secret, {
-    :site => "https://api.smugmug.com",
-    :request_token_path => '/services/oauth/1.0a/getRequestToken',
-    :access_token_path =>  '/services/oauth/1.0a/getAccessToken',
-    :authorize_path =>  '/services/oauth/1.0a/authorize',
-    :oauth_callback => 'http://localhost:4567/'
-  }
-  
-  if !session[:oauth][:request_token].nil? && !session[:oauth][:request_token_secret].nil?
-    @request_token = OAuth::RequestToken.new(@consumer, session[:oauth][:request_token], session[:oauth][:request_token_secret])
-  end
-  
-  if !session[:oauth][:access_token].nil? && !session[:oauth][:access_token_secret].nil?
-    @access_token = OAuth::AccessToken.new(@consumer, session[:oauth][:access_token], session[:oauth][:access_token_secret])
-  end
-end
-
-get "/" do
-  if @access_token
-    erb :ready
-  else
-    erb :start
-  end
-end
-
-get '/auth', { provides: 'html' } do
+get "/", { provides: 'html' } do
   haml :index
 end
 
-get "/request" do
-  @request_token = @consumer.get_request_token
-  session[:oauth][:request_token] = @request_token.token
-  session[:oauth][:request_token_secret] = @request_token.secret
-  redirect @request_token.authorize_url
+get '/upload', { provides: 'html' } do
+    haml :upload
 end
 
-get "/callback" do
-  @access_token = @request_token.get_access_token :oauth_verifier => params[:oauth_verifier]
-  session[:oauth][:access_token] = @access_token.token
-  session[:oauth][:access_token_secret] = @access_token.secret
-  redirect "/"
+post '/upload' do
+    tempfile = params[:file][:tempfile] 
+    filename = params[:file][:filename] 
+    cp(tempfile.path, "public/uploads/#{filename}")
+    redirect "/image/#{filename}"
 end
 
-get "/logout" do
-  session[:oauth] = {}
-  redirect "/"
+get '/image/:image', { provides: 'html' } do
+    @image = params[:image].to_s
+    haml :image
 end
 
-enable :inline_templates
+post '/cloudflare_upload/:image' do 
 
+  filename = params[:image]
+  
+  post = Typhoeus.post(base_url,
+    headers: {:authorization => "Bearer #{cloudflare_token}"},
+    body: {
+      id: "#{filename}#{Time.now}",
+      file: File.open("public/uploads/#{filename}","r")
+    }
+  )
+  session[:resp] = JSON.parse(post.body)
 
-__END__
+  redirect "/image/#{filename}"
 
-@@ start
-<a href="/request">PWN OAuth</a>
-
-@@ ready
-OAuth PWND. <a href="/logout">Retreat!</a>
-
+end
 
